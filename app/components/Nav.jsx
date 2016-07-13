65bdd00ecfbe7ee1e5ca7f5aff1c4a11b8b8cb44
Laura Vingada <laura@vingada.com>
2016-07-13 11:27:38 -0400

    Initialize React Timer App

---
+++ app/components/Nav.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function(){
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active-link" >Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/Countdown" activeClassName="active-link" >Countdown</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">Made By
+              <a href="http://176.32.230.22/drool.com/" target="_blank"> Laura Veee</a>
+              </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Nav;
